72465c2d81fbe8880c4ddfa23e255f17b2db0637
Fazeel Ahmed <heasdi@idj.com>
2016-11-01 12:24:56 -0700

    sending email successfully

--- ForgetPass/ForgetPass/Controllers/HomeController.cs
+++ ForgetPass/ForgetPass/Controllers/HomeController.cs
@@ -8,24 +8,49 @@ namespace ForgetPass.Controllers
 {
     public class HomeController : Controller
     {
-        int q=1;
+
+        public ActionResult sendEmail()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult sendEmail(string EnteredEmail)
+        {
+            string body;
+
+            //imagine these emails stored in db
+            ViewBag.email1 = "acb@gmail.com";
+            ViewBag.email2 = "a22cb@gmail.com";
+            ViewBag.email3 = "ac333b@gmail.com";
+            ViewBag.email4 = "ac44b@gmail.com";
+            ViewBag.email5 = "ac55b@gmail.com";
+
+            //firsrt of all we check that "EnteredEmail" is exist in our or not 
+            for (int i=0;i<=4;i++)
+            {
+                if (ViewBag.Email[i] == EnteredEmail)
+                {
+                    Email OE = new Email();
+                    OE.SendEmail(EnteredEmail,"change password request","this is msg");
+                    break;
+                }
+
+            }
+            
+
+
+            return View();
+        }
+
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
+       
     }
 }
---
+++ ForgetPass/ForgetPass/EmailClass/Email.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace ForgetPass
+{
+    public class Email
+    {
+        string smtpServer = "smtp.gmail.com";
+        int port = 587;
+        string FromEmail = "heartzkiller786@gmail.com";
+
+        public void SendEmail(String ToEmail, String Subject, string Body)
+        {
+            try
+            {
+                MailMessage MM = new MailMessage();
+
+                MM.To.Add(ToEmail);
+                // MM.Attachments.Add(new Attachment(AppDomain.CurrentDomain.BaseDirectory + "Documents/PP/abc.jpg"));
+                MM.From = new MailAddress(this.FromEmail);
+                MM.Subject = Subject;
+                MM.Body = Body;
+                SmtpClient smpt = new SmtpClient(this.smtpServer, this.port);
+                smpt.Credentials = new System.Net.NetworkCredential(this.FromEmail, "we may say");
+                smpt.EnableSsl = true;
+                smpt.Send(MM);
+            }
+
+            catch (Exception ex)
+            { }
+        }
+
+    }
+}
